4d05ea11bed6104de12923b238c09523e2a96925
Karan Aditya <karanaditya993@gmail.com>
2021-01-17 00:48:51 -0500

    feat: Add BackToTop feature

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AboutSection, HeroSection, WorkSection, ProjectsSection, Footer } from '../components'
+import { AboutSection, BackToTop, HeroSection, WorkSection, ProjectsSection, Footer } from '../components'
 
 export default () => (
   <>
@@ -10,6 +10,7 @@ export default () => (
         <WorkSection />
         <ProjectsSection />
       </div>
+      <BackToTop />
     </main>
     <Footer />
   </>
