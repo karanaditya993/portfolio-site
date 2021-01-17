ddba7fa15a90b03afe3fc0523ff8ec3ad59f8cfb
Karan Aditya <karanaditya993@gmail.com>
2021-01-16 20:22:33 -0500

    chore: Reformat index.js file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,9 @@
-export * from './about/About'
+import React from 'react'
+import { AboutSection, HeroSection } from '../components'
+
+export default () => (
+  <div className="font-serif bg-primary-dark h-full">
+    <HeroSection />
+    <AboutSection />
+  </div>
+)
--- src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { AboutSection, HeroSection } from '../components'
-
-export default () => (
-  <div className="font-serif bg-primary-dark h-full">
-    <HeroSection />
-    <AboutSection />
-  </div>
-)
